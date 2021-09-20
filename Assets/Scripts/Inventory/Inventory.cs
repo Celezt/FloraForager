b2c0663de4e4d3dbcdb137a2a945f425322e3d71
Knasse01 <62263665+Knasse01@users.noreply.github.com>
2021-09-20 22:33:17 +0200

    Item done, Updating inventory

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -8,7 +8,7 @@ public class Inventory : MonoBehaviour
     private int nrItems;
     public int gold;
     public Item[] items;
-    public bool isFull;
+    private bool isFull;
 
     public void IniFromFile()
     {
@@ -16,123 +16,135 @@ public class Inventory : MonoBehaviour
     }
     public void Initiate()
     {
-
+        items = new Item[maxSlots];
     }
     public bool AddItem(Item item)
     {
         if (!isFull) // Not full, can add
         {
-            if (item.stackable && Exists(item.id)) // Is stackable and already occupies a slot
+            if (Exists(item.id)) // Aready occupies a slot
             {
-                if (Stacking(item))
+                if (Stacking(item)) // If stacking possible
                 {
-                    return true;
-                }
-                else // Not full and does not already exist 
-                {
-                    items[FindFirstEmptySlot()] = new Item(item);
-                    return true;
+                    return true; // stacking successful
                 }
             }
-            else // Does not Exist in a slot, Add to an empty slot
+            return AddToNewSlot(item); // Adds to empty slot successfully
+        }
+        else if (Exists(item.id)) // Inventory full. Item might be stackable - check if it exists
+        {
+            return Stacking(item); // If items have been stacked or not
+        }
+
+        Debug.Log("Inventory full");
+        return false; // Item not successfully added
+    }
+    public bool RemoveAmount(int id, int amount)
+    {
+        if (Exists(id))// It exists
+        {
+
+        }
+        return false; // Does not exist or cannot remove that amount
+    }
+
+    private bool Stacking(Item item)  // Tries to stack the item
+    {
+        if (item.IsFull) // Check if you can stack the item
+        {
+            LinkedList<int> stackables = FindStackables(item.id);
+            int amountLeft = item.Amount;
+            int maxAmount = item.MaxAmount;
+            if (stackables.Count != 0) // There are stackables slots
             {
-                items[FindFirstEmptySlot()] = new Item(item);
+                for (int i = 0; i < stackables.Count; i++)
+                {
+                    int itemLeft = amountLeft - maxAmount - items[stackables.First.Value].Amount;
+                    if (0 >= itemLeft) // Fits
+                    {
+                        items[stackables.First.Value].Amount += itemLeft;                        
+                        return true;
+                    }
+                    else if (0 < itemLeft)
+                    {
+
+                    }
+                    else // itemLeft = 0
+                    {
+                        return true; // Stacked successfully
+                    }
+                    stackables.RemoveFirst();
+                }
+            }
+        }
+        return false; // Can't stack
+    }
+
+    // Done
+    private void UpdateFullness() // Check if it's full
+    {
+        if (nrItems == maxSlots) // If inventory is full 
+        {
+            isFull = true; // Is full
+        }
+        else
+        {
+            isFull = false; // Is not full
+        }
+    }
+    private bool AddToNewSlot(Item item)
+    {
+        if (!isFull) // Not full
+        {
+            items[FindFirstEmptySlot()] = new Item(item); // Does not Exist in a slot and is not stackable, Add to an empty slot
+            ++nrItems; // Added one item
+            UpdateFullness(); // If isFull == true
+            return true; // Item successfully added to one slot            
+        }
+        return false; // Is full
+    }
+    private int FindFirstEmptySlot()
+    {
+        if (!isFull) // Not full
+        {
+            for (int i = 0; i < maxSlots; i++)
+            {
+                if (items[i] == null) // Is empty
+                {
+                    return i; // Return empty slot number
+                }
+            }
+        }
+
+        return -1; // Is full
+    }
+    private bool Exists(int id) // If item exists in inventory
+    {
+        for (int i = 0; i < maxSlots; i++)
+        {
+            if (items[i].id == id)
+            {
                 return true;
             }
         }
-        else if (!item.isFull) // Inventory slots full, but slots might be stackable
-        {
-            if (Exists(item.id)) // If item exists
-            {
-
-            }
-            else
-            {
-                return false;
-            }
-
-
-            int count = 0;
-            bool foundSlot = false;
-            while (count < maxSlots || foundSlot)
-            {
-                if (items[count].id == item.id && items[count].amount + item.amount <= items[count].maxAmount)
-                {
-                    items[count].amount += item.amount;
-                }
-            }
-        }        
-        Debug.Log("Inventory full");
+        
         return false;
     }
     private LinkedList<int> FindStackables(int id) // Finding Slots with the same ID that are not full
     {
         LinkedList<int> stackables = new LinkedList<int>();
-        for (int i = 0; i < maxSlots; i++)
+        for (int i = 0; i < maxSlots; i++) // Searches through the whole array
         {
-            if (items[i].id == id && items[i].isFull != false)
+            if (items[i] != null) // If the slot is occupied
             {
-
-            }
-        }
-        return stackables; // returns 
-    }
-
-    private bool Stacking(Item item)  // Tries to stack the item
-    {
-        LinkedList<int> stackables = FindStackables(item.id);
-        int amountLeft = item.amount;
-        int maxAmount = item.maxAmount;
-        if (stackables != null)
-        {
-            for (int i = 0; i < stackables.Count; i++)
-            {
-                if (0 >  maxAmount - items[stackables.First.Value].amount - amountLeft) // Fits
+                if (items[i].id == id && items[i].IsFull != false) // Found identical item 
                 {
-                    
-                }
-                else
-                {
-
-                    return true; // Stacked successfully
-                }
-                stackables.RemoveFirst();
-            }
-        }
-        return false; // Can't stack
-    }
-    
-    private bool Exists(int id) 
-    {
-        bool found = false;
-        for (int i = 0; i < maxSlots; i++)
-        {
-            if (items[i].id == id)
-            {
-                return found;
-            }
-        }
-        
-        return found;
-    }
-    private int FindFirstEmptySlot() 
-    {
-        if (!isFull)
-        {
-            for (int i = 0; i < maxSlots; i++)
-            {
-                if (items[i] == null)
-                {
-                    return i;
+                    stackables.AddLast(i);
                 }
             }
         }
-        return -1;
+        return stackables; // returns a list filled with positions of the same stackable items 
     }
 
-    public void RemoveItem() 
-    {
-    
-    }
-    
+
 }
--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -2,21 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item
+public class Item : MonoBehaviour
 {
-    public int maxAmount;
-    public int amount;
-    public bool isFull;
-    public bool stackable;
+    private int maxAmount;
+    private int amount;
+    private bool isFull;
     public int id;
     public string itemName;
+
+    public Item(int id,string itemName, int maxAmount, int amount) 
+    {
+        this.id = id;
+        this.itemName = itemName;
+        MaxAmount = maxAmount;
+        Amount = amount;
+    }
+
+    public int Amount 
+    {
+        get { return amount; } 
+        set 
+        {
+            if (value <= 0) // Value lower or equal to zero 
+            {
+                amount = 0;
+                isFull = false;
+            }
+            else if (value >= MaxAmount) // Value is higher or equal to maxAmount
+            {
+                amount = MaxAmount; // Amount must never exceed maxAmount - Set to maxAmount instead of value
+                isFull = true; // 
+            }
+            else
+            {
+                amount = value;
+            }
+        }
+    }
+
+    public bool IsFull { get => isFull; }
+    public int MaxAmount 
+    {
+        get { return maxAmount; }
+        set
+        {
+            if (value <= 1)
+            {
+                isFull = true;
+            }
+            else
+            {
+                isFull = false;
+            }
+        }
+    }
     public Item(Item item) 
     {
-    maxAmount = item.maxAmount;
-    amount = item.amount;
-    isFull = item.isFull;
-    stackable = item.stackable;
-    id = item.id;
-    itemName = item.itemName;
+        id = item.id;
+        MaxAmount = item.MaxAmount;
+        Amount = item.Amount;    
+        itemName = item.itemName;
     }
 }
