477a58f748242adbf122bed7f9123b16581486cb
Knasse01 <62263665+Knasse01@users.noreply.github.com>
2021-09-21 14:05:32 +0200

    Updating Inventory
    Not finished but should be useable

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -10,13 +10,66 @@ public class Inventory : MonoBehaviour
     public Item[] items;
     private bool isFull;
 
+    //Done
     public void IniFromFile()
     {
 
     }
+    public bool RemoveAmount(int id, int amount)
+    {
+        LinkedList<int> positions = FindAll(id);
+        int[] tmp = new int[positions.Count];
+        int nrOfSpace = 0;
+        int amountToRemove = amount;
+        for (int i = 0; i < tmp.Length; i++)
+        {
+            nrOfSpace += items[tmp[i]].Amount;
+        }
+        if (nrOfSpace > 0 && nrOfSpace >= amount)
+        {
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                if (amountToRemove > 0)
+                {
+                    if (amountToRemove >= items[tmp[i]].Amount)
+                    {
+                        amountToRemove -= items[tmp[i]].Amount;
+                        RemoveFromSlot(tmp[i]);
+                    }
+                    else
+                    {
+                        items[tmp[i]].Amount -= amountToRemove;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        return false; // Does not exist or cannot remove that amount
+    }
+    public Inventory() 
+    {
+        items = new Item[maxSlots];
+        nrItems = 0;
+        maxSlots = 32;
+        gold = 0;
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = null;
+        }
+    }
     public void Initiate()
     {
         items = new Item[maxSlots];
+        nrItems = 0;
+        maxSlots = 32;
+        gold = 0;
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = null;
+        }
     }
     public bool AddItem(Item item)
     {
@@ -39,48 +92,41 @@ public class Inventory : MonoBehaviour
         Debug.Log("Inventory full");
         return false; // Item not successfully added
     }
-    public bool RemoveAmount(int id, int amount)
-    {
-        if (Exists(id))// It exists
-        {
-
-        }
-        return false; // Does not exist or cannot remove that amount
-    }
-
     private bool Stacking(Item item)  // Tries to stack the item
     {
-        if (item.IsFull) // Check if you can stack the item
+        if (item.IsFull) //If item is stackable
         {
-            LinkedList<int> stackables = FindStackables(item.id);
-            int amountLeft = item.Amount;
-            int maxAmount = item.MaxAmount;
-            if (stackables.Count != 0) // There are stackables slots
+            LinkedList<int> stackables = FindStackables(item); // Find how many items with the same id that have space
+            int[] tmp = new int[stackables.Count];
+            int nrOfSpace = 0;
+            if (tmp.Length > 0) // Found stackable slots
             {
-                for (int i = 0; i < stackables.Count; i++)
+                for (int i = 0; i < tmp.Length; i++) // Copy linkedlist to array
                 {
-                    int itemLeft = amountLeft - maxAmount - items[stackables.First.Value].Amount;
-                    if (0 >= itemLeft) // Fits
+                    tmp[i] = stackables.First.Value;
+                    nrOfSpace += items[stackables.First.Value].MaxAmount - items[stackables.First.Value].Amount; // Adds amount of empty space
+                    stackables.RemoveFirst();
+                }
+
+                if (nrOfSpace >= item.Amount) // There's space for item
+                {
+                    int itemLeft = item.Amount;
+                    for (int i = 0; i < tmp.Length; i++)
                     {
-                        items[stackables.First.Value].Amount += itemLeft;                        
-                        return true;
-                    }
-                    else if (0 < itemLeft)
-                    {
+                        if (0 >= itemLeft - item.MaxAmount - items[tmp[i]].Amount) // Fits
+                        {
+                            items[tmp[i]].Amount += itemLeft; // Added the remaining of the item
+                            return true;
+                        }
+                        itemLeft -= item.MaxAmount - items[tmp[i]].Amount;
+                        items[tmp[i]].Amount += item.MaxAmount - items[tmp[i]].Amount; //
 
                     }
-                    else // itemLeft = 0
-                    {
-                        return true; // Stacked successfully
-                    }
-                    stackables.RemoveFirst();
                 }
             }
         }
-        return false; // Can't stack
+        return false; // Can't stack or there is no space
     }
-
-    // Done
     private void UpdateFullness() // Check if it's full
     {
         if (nrItems == maxSlots) // If inventory is full 
@@ -103,6 +149,15 @@ public class Inventory : MonoBehaviour
         }
         return false; // Is full
     }
+    public bool RemoveFromSlot(int pos) 
+    {
+        if (items[pos] != null)
+        {
+            items[pos] = null;
+            return true;
+        }
+        return false;        
+    }
     private int FindFirstEmptySlot()
     {
         if (!isFull) // Not full
@@ -118,6 +173,26 @@ public class Inventory : MonoBehaviour
 
         return -1; // Is full
     }
+    public void Clear() 
+    {
+        items = new Item[maxSlots];
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = null;
+        }
+    }
+    private LinkedList<int> FindAll(int id) 
+    {
+        LinkedList<int> sameID = new LinkedList<int>();
+        for (int i = 0; i < maxSlots; i++)
+        {
+            if (items[i].id == id)
+            {
+                sameID.AddLast(i);
+            }
+        }
+        return sameID;
+    }
     private bool Exists(int id) // If item exists in inventory
     {
         for (int i = 0; i < maxSlots; i++)
@@ -127,19 +202,21 @@ public class Inventory : MonoBehaviour
                 return true;
             }
         }
-        
         return false;
     }
-    private LinkedList<int> FindStackables(int id) // Finding Slots with the same ID that are not full
+    private LinkedList<int> FindStackables(Item item) // Finding Slots with the same ID that are not full
     {
         LinkedList<int> stackables = new LinkedList<int>();
-        for (int i = 0; i < maxSlots; i++) // Searches through the whole array
+        if (item.MaxAmount != 1)
         {
-            if (items[i] != null) // If the slot is occupied
+            for (int i = 0; i < maxSlots; i++) // Searches through the whole array
             {
-                if (items[i].id == id && items[i].IsFull != false) // Found identical item 
+                if (items[i] != null) // If the slot is occupied
                 {
-                    stackables.AddLast(i);
+                    if (items[i].id == item.id && items[i].IsFull != false) // Found identical item 
+                    {
+                        stackables.AddLast(i);
+                    }
                 }
             }
         }
