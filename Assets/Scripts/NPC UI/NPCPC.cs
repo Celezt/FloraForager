58dc0fa1e5eb5fc31b1fb01390cd0814d2194d4b
Mumzel <mumzel99@gmail.com>
2021-09-24 00:41:14 +0200

    Added Animation and more extensive NPC texts
    .

---
+++ Assets/Scripts/NPC UI/NPCPC.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCNPC : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player") == true)
+        {
+            trigger.StartDialogue();
+
+        }
+    }
+
+
+}
