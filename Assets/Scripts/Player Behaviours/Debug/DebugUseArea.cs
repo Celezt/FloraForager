9ebb19384d3716d3e163d8cbacfd674562c1e5e3
Celezt <59172226+Celezt@users.noreply.github.com>
2021-10-06 10:18:24 +0200

    Added auto generator

--- Assets/Scripts/Player Behaviours/Debug/DebugUseArea.cs
+++ Assets/Scripts/Player Behaviours/Debug/DebugUseArea.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using System.Linq;
 
 public class DebugUseArea : MonoBehaviour, IUsable, IDestructable
 {
     public float Strength { get; set; } = DurabilityStrengths.UNARMED;
     public float Durability { get; set; } = 10;
+
+    public IList<string> Filter(ItemLabels labels) => new List<string> { labels.CROP };
 
     public void OnUse(UseContext context)
     {
