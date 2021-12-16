1b7270d87a25e4f31cc445b53d52f934d2b122af
Tobias G <31646748+Daikalos@users.noreply.github.com>
2021-12-16 15:28:27 +0100

    Updated dialogue and opened up the farm scene door

--- Assets/Scripts/Fishing/FishBaitItem.cs
+++ Assets/Scripts/Fishing/FishBaitItem.cs
@@ -13,7 +13,7 @@ public class FishBaitItem : IItem, IBait, IStar
     [OdinSerialize, PropertyOrder(int.MinValue + 2)]
     float IBait.Efficiency { get; set; } = 5;
 
-    [SerializeField]
+    [SerializeField, ListDrawerSettings(Expanded = true, AlwaysAddDefaultValue = true, ShowItemCount = false, DraggableItems = false)]
     private CellType[] _allowedUse = new CellType[] { CellType.Water };
 
     public CellType[] AllowedUse => _allowedUse;
